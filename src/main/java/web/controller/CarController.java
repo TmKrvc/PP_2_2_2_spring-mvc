992efa8317e71ef3e5c21143e662fa45a9691f96
TmKrvc <93292083+TmKrvc@users.noreply.github.com>
2022-04-26 00:23:47 +0400

    task 2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,12 +6,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
 import web.service.CarServicelmp;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    private CarServicelmp carService = new CarServicelmp();
+    private CarService carService = new CarServicelmp();
 
 
     @GetMapping
